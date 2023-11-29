acda290f858f06b01a565aaa6f992a6f29a14c29
Nursandi <sandinur157@gmail.com>
2023-11-29 08:12:35 +0700

    initial commit

---
+++ lang/en/theme_eduhub.php
@@ -0,0 +1,12 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+                                                                                                                                    
+// A description shown in the admin theme selector.                                                                                 
+$string['choosereadme'] = 'Theme EduHub is a child theme of Boost. It adds several styles to improve UI.';                
+// The name of our plugin.                                                                                                          
+$string['pluginname'] = 'EduHub Theme';                                                                                                    
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
